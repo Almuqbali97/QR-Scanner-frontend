0812396cc63371abe65df4fbc02aaf00ed1f90c0
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-11-28 17:00:55 +0400

    uodate

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegistrationForm from './components/RegistrationForm';
-import RegisteredUser from './components/RegisteredUser';
 import AdminPage from './components/AdminPage';
-// import AdminPage from './pages/admin/AdminPage';
-// import AdminPanel from './components/AdminPanel';
+
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<RegistrationForm />} />
-        <Route path="/registered" element={<RegisteredUser />} />
         <Route path="/admin" element={<AdminPage />} />
-        {/* <Route path="/admin" element={<AdminPage />} /> */}
-        {/* <Route path="/admin" element={<AdminPanel />} /> */}
+
       </Routes>
     </Router>
   );
