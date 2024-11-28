d195a3b8cd3bbd5789a13581f7a1dbfe3db3a578
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-11-28 13:02:39 +0400

    upatde

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore specific warnings that are non-critical
+        if (warning.code === 'THIS_IS_UNDEFINED') return;
+        warn(warning);
+      },
+    },
+  },
+});
