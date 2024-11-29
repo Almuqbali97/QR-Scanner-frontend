594c077d4b9ea890deb1f69b80a8d05b4807aee7
Musaab Almuqbali <almuqbalimusab@gmail.com>
2024-11-29 17:01:25 +0400

    update user xp

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -12,19 +12,24 @@ const RegistrationForm = () => {
         isGroup: false,
     });
 
+    const [isLoading, setIsLoading] = useState(false);
     const [qrCodeData, setQrCodeData] = useState(null); // Success data for QR
     const [responseMessage, setResponseMessage] = useState(''); // To store the response message
 
     const handleSubmit = async () => {
+        setIsLoading(true); // Start loading before validation and API call
+
         const { firstName, lastName, mobileNumber, groupSize } = formData;
 
         // Validation
         if (!firstName || !lastName || !mobileNumber) {
             setResponseMessage('All fields are required!');
+            setIsLoading(false); // Stop loading if validation fails
             return;
         }
         if (mobileNumber.length < 8) {
             setResponseMessage('Mobile number must be at least 8 digits long!');
+            setIsLoading(false); // Stop loading if validation fails
             return;
         }
 
@@ -35,6 +40,7 @@ const RegistrationForm = () => {
                 mobileNumber,
                 groupSize: formData.isGroup ? groupSize : 1,
             };
+
             const response = await axios.post(import.meta.env.VITE_API_URL + "/register", payload);
 
             setQrCodeData({
@@ -57,40 +63,57 @@ const RegistrationForm = () => {
             setResponseMessage(
                 error.response?.data?.message || 'An error occurred during registration.'
             );
+        } finally {
+            setIsLoading(false); // Always stop loading after the API call
         }
     };
+
 
     return (
         <div className="h-screen flex justify-center items-center">
             <div>
-                <div className='flex w-full  mt-9 sm:mt-20 justify-start items-center'>
-                    <img src={logo} className=' w-56 sm:w-80 mx-auto' />
+                <div>
+
+                    <div className='flex w-full mt-3 sm:mt-20 justify-start items-center'>
+                        <img src={logo} className=' w-56 sm:w-80 mx-auto' />
+                    </div>
                 </div>
-                <div className="p-6 max-w-lg min-w-[350px] mx-auto bg-gradient-to-r from-blue-50 to-green-50 shadow-xl rounded-lg">
-                    <h2 className="text-3xl font-extrabold text-center mb-6 text-gray-800">
-                        New Registration
-                    </h2>
-                    {!qrCodeData ? (
-                        <RegistrationFormFields
-                            formData={formData}
-                            setFormData={setFormData}
-                            handleSubmit={handleSubmit}
-                        />
-                    ) : (
-                        <RegistrationSuccessCard
-                            qrCodeData={qrCodeData}
-                            onDownload={() => {
-                                // Define download logic here if needed
-                            }}
-                            onGoBack={() => setQrCodeData(null)}
-                        />
-                    )}
-                    {responseMessage && (
-                        <p className="mt-4 text-center text-sm font-semibold text-red-600">
-                            {responseMessage}
-                        </p>
-                    )}
-                </div>
+                {isLoading ?
+
+                    <div role="status" className=' h-full w-full flex justify-center items-center mt-5'>
+                        <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                        </svg>
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                    :
+                    <div className="p-6 max-w-lg min-w-[350px] mx-auto bg-gradient-to-r from-blue-50 to-green-50 shadow-xl rounded-lg">
+                        <h2 className="text-3xl font-extrabold text-center mb-6 text-gray-800">
+                            New Registration
+                        </h2>
+                        {!qrCodeData ? (
+                            <RegistrationFormFields
+                                formData={formData}
+                                setFormData={setFormData}
+                                handleSubmit={handleSubmit}
+                            />
+                        ) : (
+                            <RegistrationSuccessCard
+                                qrCodeData={qrCodeData}
+                                onDownload={() => {
+                                    // Define download logic here if needed
+                                }}
+                                onGoBack={() => setQrCodeData(null)}
+                            />
+                        )}
+                        {responseMessage && (
+                            <p className="mt-4 text-center text-sm font-semibold text-red-600">
+                                {responseMessage}
+                            </p>
+                        )}
+                    </div>}
+
             </div>
         </div>
     );
